52a9ec8c272f199d4bc65f74c6f64ac063e42f12
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-12-25 14:14:07 +0330

    complete about page

--- src/components/About/CommentBox/CommentBox.js
+++ src/components/About/CommentBox/CommentBox.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { FaStar } from "react-icons/fa";
-export default function CommentBox({name,content,score}) {
+import React from "react"
+import { FaStar, FaRegStar } from "react-icons/fa"
+export default function CommentBox({ name, content, score }) {
+  console.log(score);
   return (
-    <div className='flex flex-col items-center gap-9'>
-        <h5 className='text-3xl text-center'>،، {content} ،،</h5>
-         <div className='flex text-secondary'>
-         <FaStar />
-         <FaStar />
-         <FaStar />
-         <FaStar />
-         <FaStar />
-         </div>
-         <p>{name}</p>
+    <div className="flex flex-col items-center sm:gap-5 md:gap-9">
+      <h5 className="md:text-2xl text-center">"{content}"</h5>
+      <div className="flex text-info">
+        {Array(5 - score)
+          .fill("0")
+          .map((item, index) => (
+            <FaRegStar />
+          ))}
+        {Array(score)
+          .fill(1)
+          .map((item, index) => (
+            <FaStar />
+          ))}
+      </div>
+      <p className="text-zinc-600 sm:text-sm md:text-base">{name}</p>
     </div>
   )
 }
