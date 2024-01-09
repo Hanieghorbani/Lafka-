56a6557d18d53e2ba19771f1d0be15a437a5e731
hanie ghorbani <hnie.ghorbani@gmail.com>
2024-01-09 18:01:23 +0330

    remove some photos

--- src/components/Main/Category/Category.js
+++ src/components/Main/Category/Category.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 export default function Category({categorys}) {
   return (
-    <div className="lg:w-3/4 bg-primary sm:mt-40 lg:mt-[11.5rem] container-primary p-3 rounded-b-3xl mt-60 text-sm">
+    <div className="lg:w-3/4 bg-primary sm:mt-40 md:mt-44 lg:mt-[11.2rem] container-primary p-3 rounded-b-3xl text-sm">
       <div className="lg:w-1/2 text-white flex items-center justify-between mx-auto">
         {categorys.map((category) => (
           <Link
